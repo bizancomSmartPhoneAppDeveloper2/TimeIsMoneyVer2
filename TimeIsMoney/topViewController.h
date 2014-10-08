e9fd5f171ef04ef9ca6b33e445630c9b49b7a054
ビザンコムマック　13 <bizan.com.mac13@bizankomumakku15-no-MacBook-Air.local>
2014-10-08 10:03:58 +0900

    単独のプロジェクトがテーブルビューに入るようになった

---
+++ TimeIsMoney/topViewController.h
@@ -0,0 +1,13 @@
+//
+//  topViewController.h
+//  TimeIsMoney
+//
+//  Created by ビザンコムマック　13 on 2014/10/08.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface topViewController : UIViewController
+
+@end
---
+++ TimeIsMoney/topViewController.m
@@ -0,0 +1,37 @@
+//
+//  topViewController.m
+//  TimeIsMoney
+//
+//  Created by ビザンコムマック　13 on 2014/10/08.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import "topViewController.h"
+
+@interface topViewController ()
+
+@end
+
+@implementation topViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
